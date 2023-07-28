6cfd55020b1a2827b62c10c1a2737c61ba0587fd
sole <listas@soledadpenades.com>
2023-07-28 08:52:26 +0100

    humble beginnings...

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Plugin Name:       Reliably HEIC
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Reliably support HEIC files in WordPress
+ * Version:           1.0.0
+ * Author:            Soledad Penadés
+ * Author URI:        https://soledadpenades.com/
+ * Text Domain:       reliably-heic
+ */

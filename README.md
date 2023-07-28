69184c4645bb13ee985429df6066a2c4c43f05d0
sole <listas@soledadpenades.com>
2023-07-28 10:43:00 +0100

    Add requirements check

---
+++ README.md
@@ -0,0 +1,8 @@
+# Reliably HEIC
+
+## System requirements
+
+### ImageMagick extension present and activated
+
+Uncomment `extension=imagick.so` in `php.ini`. You probably also need to restart the server.
+(If using MAMP to develop locally, php.ini is in `/Applications/MAMP/bin/php/{the version of php you're using}/conf/php.ini`).
--- ReliablyHEICPlugin.php
+++ ReliablyHEICPlugin.php
@@ -4,6 +4,7 @@ if(!class_exists('ReliablyHEICPlugin')) {
 
 	class ReliablyHEICPlugin {
 		public function setup() {
+			//add_action('admin_init', array($this, 'add_admin_settings'));
 			add_action('admin_menu', array($this, 'setup_admin_menu'));
 		}
 
@@ -27,13 +28,73 @@ if(!class_exists('ReliablyHEICPlugin')) {
 				return;
 			}
 
+			$checks = $this->run_through_system_requirements();
+			$satisfied = $this->are_system_requirements_satisfied($checks);
+
 			?>
 			<div class="wrap">
 			<h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-			hello =)
+			<h2>Requirements check</h2>
+			<?php if($satisfied) { ?>
+				All in place: the plugin can work correctly!
+			<?php } else {
+				?>
+				<p>Requirements missing:</p>
+				<ul>
+				<?php
+				foreach($checks as $check) {
+					if(isset($check['error'])) {
+						echo '<li><strong>' . $check['desc'] .':</strong> ' . $check['error'] . '</li>';
+					}
+				}
+				?>
+				</ul>
+				<?php
+			}
+			?>
+			
 			<?php
 		}
 
+		protected function run_through_system_requirements() {
+			$checks = array(
+				array(
+					'desc' => 'ImageMagick extension present',
+					'func' => 'validate_imagemagick'
+				)
+			);
+
+			foreach($checks as &$check) {
+				$callable = array($this, $check['func']);
+				$result = call_user_func($callable);
+				if(isset($result['error'])) {
+					$check['error'] = $result['error'];
+				} else {
+					$check['ok'] = true;
+				}
+			}
+
+			return $checks;
+		}
+
+		protected function are_system_requirements_satisfied($checks) {
+			foreach($checks as $check) {
+				if(isset($check['error'])) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		protected function validate_imagemagick() {
+			$result = array();
+			if(class_exists('Imagick')) {
+				$result['ok'] = true;
+			} else {
+				$result['error'] = 'Imagick class does not exist. Did you activate the ImageMagick PHP module in your server? (<tt>extension=imagick.so</tt> in <tt>php.ini</tt>)';
+			}
+			return $result;
+		}
 
 	}
 
